203dfaa01ed720062cdd90098fc5fdd65caefb8e
Alexander Kvamme <alexanderkvamme@gmail.com>
2019-08-01 10:19:37 +0200

    add SettingsViewController to a ScrollableStackContainer

--- BottomSheet/Factories/ModuleFactoryImp.swift
+++ BottomSheet/Factories/ModuleFactoryImp.swift
@@ -9,7 +9,6 @@ final class ModuleFactoryImp:
 {
     func makeDetailedTransactionModule(router: Router) -> DetailedTransactionView {
         let zoomableReceiptViewController = ZoomableReceiptViewController()
-//        let rootSheet = MatchfinderRootSheet(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil, router: Router)
         let rootSheet = MatchfinderRootSheet(router: router)
         let bottomSheetContainer = ScrollableBottomSheetContainer(rootSheet)
         let container = DetailedTransactionViewController(mainViewController: zoomableReceiptViewController, sheetViewController: bottomSheetContainer)
@@ -22,7 +21,6 @@ final class ModuleFactoryImp:
 
     func makeCardTransactionsModule(router: Router) -> CardTransactionsView {
         return CardTransactionsViewController()
-//        return wrapController()
     }
 }
 
--- BottomSheet/Flows/Settings flow/SettingsViewController.swift
+++ BottomSheet/Flows/Settings flow/SettingsViewController.swift
@@ -8,9 +8,118 @@
 
 import Foundation
 import UIKit
+import ScrollingStackContainer
 
 
-final class SettingsViewController: UIViewController, SettingsView {
+extension SKScreenHeaderController: StackContainable {
+    func preferredAppearanceInStack() -> ScrollingStackController.ItemAppearance {
+        return ScrollingStackController.ItemAppearance.view(height: 120 )
+    }
+}
+
+final class SKScreenHeaderController: UIViewController {
+
+    // MARK: - Properties
+    
+    private let headerLabel = UILabel()
+    private let subheaderLabel = UILabel()
+    
+    // MARK: - Initializers
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        setup()
+        addSubviewsAndConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    private func setup() {
+        headerLabel.text = "Innstillinger"
+        headerLabel.textColor = UIColor.solarstein.sapphire
+        headerLabel.font = UIFont.kolibrix.header
+        
+        subheaderLabel.text = "Her kan du justere dine preferanser"
+        subheaderLabel.textColor = UIColor.solarstein.sapphire
+        subheaderLabel.alpha = 0.3
+        subheaderLabel.font = UIFont.kolibrix.body
+    }
+    
+    private func addSubviewsAndConstraints() {
+        [headerLabel, subheaderLabel].forEach({ view.addSubview($0) })
+        
+        headerLabel.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.left.equalToSuperview().offset(40).priority(750)
+            make.right.equalToSuperview()
+        }
+        
+        subheaderLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(headerLabel)
+            make.top.equalTo(headerLabel.snp.bottom)
+            make.right.equalToSuperview()
+        }
+    }
+}
+
+
+extension SettingsTableController: StackContainable {
+    func preferredAppearanceInStack() -> ScrollingStackController.ItemAppearance {
+        return ScrollingStackController.ItemAppearance.scroll(tableView, insets: UIEdgeInsets.zero)
+    }
+}
+
+final class SettingsTableController: UIViewController {
+
+    // MARK: - Properties
+    
+    private var tableView = UITableView(frame: .zero, style: .grouped)
+    private var dataSourceAndDelegate = SettingsDataSourceAndDelegate()
+    private let tableViewShadow = ShadowView()
+    
+    // MARK: - Initializers
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        setup()
+        addSubviewsAndConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    private func setup() {
+        setupTableView()
+    }
+    
+    private func addSubviewsAndConstraints() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    private func setupTableView() {
+        tableView.dataSource = dataSourceAndDelegate
+        tableView.delegate = dataSourceAndDelegate
+        tableView.showsVerticalScrollIndicator = false
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
+        tableView.separatorColor = UIColor.solarstein.sapphire
+        tableView.clipsToBounds = true
+    }
+}
+
+final class SettingsViewController: ScrollingStackController, SettingsView {
     
     // MARK: - Properties
 
@@ -18,12 +127,9 @@ final class SettingsViewController: UIViewController, SettingsView {
     
     var coordinator: Coordinator!
     
-    private let xButton = UIButton()
-    private let headerLabel = UILabel()
-    private let subheaderLabel = UILabel()
-    private var tableView = UITableView(frame: .zero, style: .grouped)
-    private var dataSourceAndDelegate = SettingsDataSourceAndDelegate()
-    private let tableViewShadow = ShadowView()
+    private let customNav = SKTopNavigationController()
+    private let headerController = SKScreenHeaderController()
+    private let tableController = SettingsTableController()
     
     // MARK: - Initializers
     
@@ -41,77 +147,30 @@ final class SettingsViewController: UIViewController, SettingsView {
     override func viewDidLoad() {
         setup()
         addSubviewsAndConstraints()
+        
+        viewControllers = [customNav, headerController, tableController] as [StackContainable]
     }
     
     // MARK: - Methods
-
+    
     private func setup() {
-        setupXButton()
-        setupLabels()
-        setupTableView()
-    }
-    
-    private func setupXButton() {
-        xButton.setImage(UIImage(named: "x-icon")?.withRenderingMode(.alwaysTemplate), for: .normal)
-        xButton.addTarget(self, action: #selector(finish), for: .touchUpInside)
-        xButton.tintColor = UIColor.solarstein.sapphire
-        xButton.alpha = 0.1
-    }
-    
-    private func setupLabels() {
-        headerLabel.text = "Innstillinger"
-        headerLabel.textColor = UIColor.solarstein.sapphire
-        headerLabel.font = UIFont.kolibrix.header
+        setupScrollView()
         
-        subheaderLabel.text = "Her kan du justere dine preferanser"
-        subheaderLabel.textColor = UIColor.solarstein.sapphire
-        subheaderLabel.alpha = 0.3
-        subheaderLabel.font = UIFont.kolibrix.body
+        customNav.topLeftNavigationButton.addTarget(self, action: #selector(finish), for: .touchUpInside)
     }
     
-    private func setupTableView() {
-        tableView.dataSource = dataSourceAndDelegate
-        tableView.delegate = dataSourceAndDelegate
-        tableView.showsVerticalScrollIndicator = false
-        tableView.backgroundColor = .clear
-        tableView.separatorStyle = .none
-        tableView.separatorColor = UIColor.solarstein.sapphire
-        tableView.clipsToBounds = true
+    private func setupScrollView() {
+        scrollView = UIScrollView(frame: .zero)
+        scrollView?.showsVerticalScrollIndicator = false
+        scrollView?.backgroundColor = .white
     }
     
     private func addSubviewsAndConstraints() {
-        [tableViewShadow, headerLabel, subheaderLabel, tableView, xButton, tableView].forEach({ view.addSubview($0) })
+        guard let scrollView = scrollView else { fatalError() }
         
-        tableViewShadow.backgroundColor = .clear
-        
-        tableViewShadow.snp.makeConstraints { (make) in
-            make.top.equalTo(tableView.snp.top)
-            make.left.right.bottom.equalToSuperview()
-        }
-        
-        headerLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(xButton.snp.bottom).offset(24)
-            make.left.equalToSuperview().offset(40)
-            make.right.equalToSuperview()
-        }
-        
-        subheaderLabel.snp.makeConstraints { (make) in
-            make.left.equalTo(headerLabel)
-            make.top.equalTo(headerLabel.snp.bottom)
-            make.right.equalToSuperview()
-        }
-        
-        xButton.snp.makeConstraints { (make) in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin).offset(16)
-            make.left.equalTo(view.safeAreaLayoutGuide.snp.leftMargin).offset(32+Device.additionalInsetsIfNotched)
-            make.height.width.equalTo(48)
-        }
-        
-        tableView.snp.makeConstraints { (make) in
-            make.right.equalToSuperview().offset(-16)
-            make.left.equalToSuperview().offset(16)
-            make.top.equalTo(subheaderLabel.snp.bottom).offset(40)
-            make.bottom.equalToSuperview().offset(-16)
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
         }
     }
     
